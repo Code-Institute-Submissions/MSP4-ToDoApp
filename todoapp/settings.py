f0773b627e6a6aa8b4e702df795636f330ce8f91
Steve Alsamarae <sarsaif@yahoo.com>
2021-11-23 04:14:53 +0000

    Remove secret key

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_znxb+&t15^0*o%809n%v8-9brmv*u=!f!c$$6(5sgg396c%1e')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
