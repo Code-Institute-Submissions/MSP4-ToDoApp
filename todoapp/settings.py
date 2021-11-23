778b9e0e06480f4ca0b6c120cb41e96f16bf561d
Steve Alsamarae <sarsaif@yahoo.com>
2021-11-23 04:08:18 +0000

    setup development enviroment

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_znxb+&t15^0*o%809n%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
